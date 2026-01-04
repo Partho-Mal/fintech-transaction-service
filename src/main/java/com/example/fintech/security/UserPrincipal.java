0b408c4936369d5c40a09ac30707ffef196334b7
Partho <parthomal7@gmail.com>
2026-01-04 12:38:52 +0530

    feat: jwt security, rbac, and structured logging (Day 5)

---
+++ src/main/java/com/example/fintech/security/UserPrincipal.java
@@ -0,0 +1,60 @@
+//UserPrincipal
+//username
+//role
+//implements UserDetails
+
+package com.example.fintech.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserPrincipal implements UserDetails {
+
+    private final String username;
+    private final Role role;
+
+    public UserPrincipal(String username, Role role) {
+        this.username = username;
+        this.role = role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
+
